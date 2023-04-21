7b73835bf3cbd1ca0dce48a0fa774b9e802713d3
mwaniel <mwaniel03@gmail.com>
2023-04-21 09:06:35 +0300

    new login

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -32,7 +32,19 @@
     align-items:center;
     width:22.8em;
     height:55em;
-    margin:1em auto 0 auto;
+    margin:0 auto 0 auto;
     overflow:hidden;
+}
+input{
+    background:rgba(228, 232, 243,0.8);
+    background-position:0.5em 0.6em;
+    border:none;
+    color:rgba(80, 80, 80, 1);
+    padding:0 0 0 4rem;
+    margin:0 0 1em 0;
+    width: 20em;
+    height:2.8em;
+    outline:none;
+    transition:background-color 0.4s;
 }
 </style>
