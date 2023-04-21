d479acb31bd832d9c05345bf8d988ca37b3674bf
mwaniel <mwaniel03@gmail.com>
2023-04-21 09:47:16 +0300

    auth login

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -1,12 +1,36 @@
+<script setup>
+    import {reactive, ref} from 'vue'
+    import {useRouter} from 'vue-router'
+
+    const router = useRouter()
+
+    let form = reactive({
+        email:'',
+        password:'',
+    })
+    let error= ref('')
+
+    const login = async()=>{
+        await axios.post('/api/login', form)
+            .then(response =>{
+                if (response.data.success){
+                    localStorage.setItem('token',response.data.data.token)
+                }else{
+                    error.value = response.data.message;
+                }
+            })
+    }
+</script>
 <template>
     <div class="login">
         <div class="formLogin">
-            <form>
-            <input type="email" placeholder="Enter your Email"/>
+            <p class="text-danger" v-if="error">{{error}}</p>
+            <form @submit.prevent="login">
+            <input type="email" placeholder="Enter your Email" v-model="form.email"/>
             <br>
-            <input type="password" placeholder="Enter your Password"/>
+            <input type="password" placeholder="Enter your Password" v-model="form.password"/>
             <br>
-            <input type="submit" value="login"/>
+            <input type="submit" value="login" class="submit"/>
         </form>
         </div>
     </div>
@@ -46,5 +70,33 @@ input{
     height:2.8em;
     outline:none;
     transition:background-color 0.4s;
+}
+input:hover{
+    background-color:rgba(255, 255, 255, 255);
+}
+input:focus{
+    background-color:rgba(255, 255, 255, 255);
+}
+.submit{
+    background:rgba(220, 220, 220,1);
+    color:rgba(35, 35, 35, 0.8);
+    padding:0;
+    margin:2.5em 0 0 5em;
+    width: 10em;
+    text-transform:uppercase;
+    cursor:pointer;
+    transition:background-color 0.4s;
+}
+.submit:hover {
+    background:#43467f;
+    color:#ffffff;
+}
+.submit:focus {
+    background:#43467f;
+    color:#ffffff;
+}
+.text-danger{
+    color:red;
+    font-size:16px;
 }
 </style>
